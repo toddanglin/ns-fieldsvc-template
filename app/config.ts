8b65e9252eced6780a5b7b6751b503ce26f3cc08
Todd Anglin <toddanglin@gmail.com>
2018-02-10 19:23:06 -0600

    Updated file used to store API keys

---
+++ app/config.ts
@@ -0,0 +1,7 @@
+export class Config {
+    static GOOGLE_MAPS_KEY = "YOUR_API_KEY";
+    static KINVEY_APP_KEY = "YOUR_API_KEY";
+    static KINVEY_APP_SECRET = "YOUR_API_KEY";
+    static STRIPE_TEST_KEY = "YOUR_API_KEY";
+    static STRIPE_PROD_KEY = "YOUR_API_KEY";
+}
